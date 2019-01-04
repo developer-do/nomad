277479a2a34e32d269b56b622c7e4da25123eb74
developer-do <dev-do@naver.com>
2019-01-05 03:14:02 +0900

    27강 completegit add -A!

---
+++ JS_basic/todo.js
@@ -0,0 +1,56 @@
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintTodo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "x";
+  span.innerText = text;
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  li.id = newId;
+  toDoList.appendChild(li);
+
+  const toDoObj = {
+    text: text,
+    id: newId
+  }
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintTodo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    console.log(parsedToDos);
+    parsedToDos.forEach(function (toDo) {
+      paintTodo(toDo.text);
+    });
+  }
+}
+
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+init();
